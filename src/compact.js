eaed5210c3ffdf9a6feab3509abdc13e140019f2
shinya takahashi <s.takahashi313@gmail.com>
2014-06-25 10:16:28 +0900

    add storaging

--- src/compact.js
+++ src/compact.js
@@ -3,10 +3,12 @@ var compact = {};
 var utils = require("./utils.js"),
     core = require("./core.js"),
     finder = require("./finder.js"),
-    writer = require("./writer.js");
+    writer = require("./writer.js"),
+    store = require("./store.js");
 
 utils.extend(compact, core);
 utils.extend(compact, finder);
 utils.extend(compact, writer);
+utils.extend(compact, store);
 
 module.exports = compact;
---
+++ src/store.js
@@ -0,0 +1,16 @@
+var localforage = require("localforage");
+
+module.exports = {
+
+  saveDb: function(callback) {
+    var key = this._storageKey,
+        item = JSON.stringify(this._records);
+    localforage.setItem(key, item, callback);
+  },
+
+  loadDb: function(callback) {
+    var key = this._storageKey;
+    localforage.getItem(key, callback);
+  }
+
+};
